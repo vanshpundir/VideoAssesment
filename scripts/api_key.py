70f1e468b5b4e06407b8481d4e8342b8c55fe816
vanshpundir <vansh@Vanshs-MacBook-Air-3.local>
2023-09-09 12:56:25 +0530

    updated some files;

---
+++ scripts/api_key.py
@@ -0,0 +1,4 @@
+import os
+
+# Set the path to your service account key JSON file
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/vansh/Downloads/client_secret_131786200640-teknr7c09pkhmnrjt2tu6q9u9bbcu5l1.apps.googleusercontent.com.json"
